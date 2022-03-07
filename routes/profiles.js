445202536daae55f5f80c68bcd51b87b4c92aefb
choijyoung <choi.jae.y@gmail.com>
2022-03-07 13:39:55 -0500

    profile controller

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { isLoggedIn } from "../middleware/middleware.js";
+import * as profilesCtrl from '../controllers/profiles.js'
 
 const router = Router()
 
 // GET 
-router.get('/', isLoggedIn)
+router.get('/', isLoggedIn, profilesCtrl.index)
 
 
 
