95fc0a7b2e9af6fe767dc710bd083571864d3e32
choijyoung <choi.jae.y@gmail.com>
2022-03-09 18:15:45 -0500

    add favicon

--- models/profile.js
+++ models/profile.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
+const Schema = mongoose.Schema;
 
 const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
+  myMusic: [{type: Schema.Types.ObjectId, ref:"Song"}],
 }, {
   timestamps: true
 })
